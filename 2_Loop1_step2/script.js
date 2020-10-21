791a730a019148f2924c887af98038c0611bf871
ireneusz <ireneusz.petryszak@gmail.com>
2020-10-21 15:32:13 +0200

    added js

---
+++ 2_Loop1_step2/script.js
@@ -0,0 +1,41 @@
+
+const kindOfContent = ['Positive content', 'Negative content', 'Neutral content'];
+
+const changeButton = document.querySelector('.tab__button');
+const buttonsNoFrame = document.querySelectorAll('.tab__button--no-frame');
+
+changeButton.addEventListener('click',textButtonHandler);
+buttonsNoFrame.forEach(btn => btn.addEventListener('click',noFrameButtonHandler));
+
+function textButtonHandler(event) {
+    const textElement = document.querySelector('.tab__text');
+    const number = textElement.value - 1;
+    textElement.value = '';
+
+    if( number > 2 || number < 0 ) {
+        alert('Invalid number - please insert number in range of 1 - 3');
+    }
+    else {
+        onFocused(number);
+        changeText(number);
+    }
+}
+
+function noFrameButtonHandler(event) {
+    buttonsNoFrame.forEach((obj,index) => {
+        if(obj === this) {
+            onFocused(this);
+            changeText(index);
+        }
+    })
+}
+
+function changeText(number) {
+    document.querySelector('.tab__text-display').textContent = kindOfContent[number];
+}
+
+function onFocused(element) {
+    buttonsNoFrame.forEach(btn => btn.classList.remove("tab__button--no-frame--active"));
+    if(typeof element === 'object') element.classList.add("tab__button--no-frame--active");
+    if(typeof element === 'number') buttonsNoFrame[element].classList.add("tab__button--no-frame--active");
+}
